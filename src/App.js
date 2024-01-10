0889799cf0563da470576b55a4a31980c5e7f16b
Susovan <susovan.sahoo777@gmail.com>
2024-01-10 12:53:11 +0530

    Created first react app and wrote a advice code by fetching API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+// import "./styles.css";
 
-function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <h2>{advice}</h2>
+
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice.
+      </p>
     </div>
   );
 }
-
-export default App;

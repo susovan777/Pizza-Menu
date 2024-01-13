196ed3dc3ea265921e18bbb84fb5556bb4a2fd76
Susovan <susovan.sahoo777@gmail.com>
2024-01-14 02:01:42 +0530

    Added Pizza component inside App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ export default function App() {
   return (
     <div>
       <h1>Hello World! This is cloned</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
     </div>
   );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" />
+      <h2>Pizza</h2>
+    </div>
+  )
 }

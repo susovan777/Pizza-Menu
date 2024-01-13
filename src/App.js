0c3b0af05dae7db288fd56fdbe9b61106fde7912
Susovan <susovan.sahoo777@gmail.com>
2024-01-14 02:06:57 +0530

    Modified App component: Added Header and Footer component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ export default function App() {
 
   return (
     <div>
-      <h1>Hello World! This is cloned</h1>
+      <Header />
+      <h1>Hello World!</h1>
       <Pizza />
       <Pizza />
       <Pizza />
+      <Footer />
     </div>
   );
 }
@@ -18,4 +20,16 @@ function Pizza() {
       <h2>Pizza</h2>
     </div>
   )
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Footer() {
+return <footer>{new Date().toLocaleTimeString()}. We are currently open.</footer>
+}
+
+function Menu() {
+
 }

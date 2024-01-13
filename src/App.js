08960cd05f5f51025f214e4a9e881d3dfcae7e80
Susovan <susovan.sahoo777@gmail.com>
2024-01-14 01:31:00 +0530

    added starter files; cloned from another react app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
-// import "./styles.css";
 
 export default function App() {
-  const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
-
-  async function getAdvice() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
-    console.log(data.slip.advice);
-    setAdvice(data.slip.advice);
-    setCount((count) => count + 1);
-  }
-
-  useEffect(() => {
-    getAdvice();
-  }, []);
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <h2>{advice}</h2>
-
-      <button onClick={getAdvice}>Get Advice</button>
-      <p>
-        You have read <strong>{count}</strong> pieces of advice.
-      </p>
+      <h1>Hello World! This is cloned</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-/* 
-  👉 Strict Mode enables the following checks in development:
-      - Your components will re-render an extra time to find bugs caused by impure rendering.
-      - Your components will re-run Effects an extra time to find bugs caused by missing Effect cleanup.
-      - Your components will be checked for usage of deprecated APIs.
-*/
+
 root.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-/* 🚩🚩
-// React v18
-const root = ReactDOM.createElement(document.getElementById("root"));
-root.render(<App />);
-
-// before v18
-React.render(<App />);
-*/
-
-
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
